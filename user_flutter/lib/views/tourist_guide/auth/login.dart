c8b790298bca9ead7dbffbbec2410b46d78adf9b
Mamunur Rashid Mamun <mrmamun20162017@gmail.com>
2023-01-27 16:33:47 +0600

    neareby area found for serching tourest guide

--- user_flutter/lib/views/tourist_guide/auth/login.dart
+++ user_flutter/lib/views/tourist_guide/auth/login.dart
@@ -51,93 +51,96 @@ class _GuideLoginState extends State<GuideLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.blue),
-        child: Form(
-          key: formKey,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Image(
-                    image: AssetImage('images/logos/logo_white.png'),
-                    height: 70),
-                const SizedBox(height: 20),
-                const Text('Tourist Guide Login',
-                    style: TextStyle(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(color: Colors.blue),
+          height: MediaQuery.of(context).size.height,
+          child: Form(
+            key: formKey,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(
+                      image: AssetImage('images/logos/logo_white.png'),
+                      height: 70),
+                  const SizedBox(height: 20),
+                  const Text('Tourist Guide Login',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 30),
+                  Container(
+                    height: 310,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
                         color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold)),
-                const SizedBox(height: 30),
-                Container(
-                  height: 310,
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          offset: const Offset(0, 5),
-                          blurRadius: 10,
-                        )
-                      ]),
-                  child: ListView(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: TextFormField(
-                          controller: emailController,
-                          validator: (value) =>
-                              value!.isEmpty ? 'Email is required' : null,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: inputDecoration('Email'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: TextFormField(
-                          controller: passwordController,
-                          validator: (value) => value!.length < 8
-                              ? 'Password at least 8 character'
-                              : null,
-                          obscureText: true,
-                          decoration: inputDecoration('Password'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: submitBtn('LOGIN', loading, () {
-                          if (formKey.currentState!.validate()) {
-                            setState(() {
-                              loading = true;
-                              _loginGuide();
-                            });
-                          }
-                        }),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Don\'t have an account?'),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pushAndRemoveUntil(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const GuideRegister()),
-                                  (route) => false);
-                            },
-                            child: const Text('Register'),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            offset: const Offset(0, 5),
+                            blurRadius: 10,
+                          )
+                        ]),
+                    child: ListView(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: TextFormField(
+                            controller: emailController,
+                            validator: (value) =>
+                                value!.isEmpty ? 'Email is required' : null,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: inputDecoration('Email'),
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: TextFormField(
+                            controller: passwordController,
+                            validator: (value) => value!.length < 8
+                                ? 'Password at least 8 character'
+                                : null,
+                            obscureText: true,
+                            decoration: inputDecoration('Password'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: submitBtn('LOGIN', loading, () {
+                            if (formKey.currentState!.validate()) {
+                              setState(() {
+                                loading = true;
+                                _loginGuide();
+                              });
+                            }
+                          }),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Don\'t have an account?'),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const GuideRegister()),
+                                    (route) => false);
+                              },
+                              child: const Text('Register'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- user_flutter/lib/views/tourist_guide/auth/register.dart
+++ user_flutter/lib/views/tourist_guide/auth/register.dart
@@ -51,124 +51,128 @@ class _GuideRegisterState extends State<GuideRegister> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.blue),
-        child: Form(
-          key: formKey,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Image(
-                    image: AssetImage('images/logos/logo_white.png'),
-                    height: 70),
-                const SizedBox(height: 20),
-                const Text('Tourist Guide Login',
-                    style: TextStyle(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(color: Colors.blue),
+          height: MediaQuery.of(context).size.height,
+          child: Form(
+            key: formKey,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(
+                      image: AssetImage('images/logos/logo_white.png'),
+                      height: 70),
+                  const SizedBox(height: 20),
+                  const Text('Tourist Guide Regisdter',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 30),
+                  Container(
+                    height: 520,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
                         color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold)),
-                const SizedBox(height: 30),
-                Container(
-                  height: 520,
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          offset: const Offset(0, 5),
-                          blurRadius: 10,
-                        )
-                      ]),
-                  child: ListView(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: TextFormField(
-                            controller: nameController,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            offset: const Offset(0, 5),
+                            blurRadius: 10,
+                          )
+                        ]),
+                    child: ListView(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: TextFormField(
+                              controller: nameController,
+                              validator: (value) =>
+                                  value!.isEmpty ? 'Name is required' : null,
+                              decoration: inputDecoration('name')),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: TextFormField(
+                            controller: phoneController,
                             validator: (value) =>
-                                value!.isEmpty ? 'Name is required' : null,
-                            decoration: inputDecoration('name')),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: TextFormField(
-                          controller: phoneController,
-                          validator: (value) =>
-                              value!.isEmpty ? 'Phone is required' : null,
-                          keyboardType: TextInputType.phone,
-                          decoration: inputDecoration('Phone'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: TextFormField(
-                          controller: emailController,
-                          validator: (value) =>
-                              value!.isEmpty ? 'Email is required' : null,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: inputDecoration('Email'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: TextFormField(
-                          controller: passwordController,
-                          validator: (value) =>
-                              value!.isEmpty ? 'Password is required' : null,
-                          obscureText: true,
-                          decoration: inputDecoration('Password'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: TextFormField(
-                          validator: (value) => value!.isEmpty
-                              ? 'Re-enter the password'
-                              : value != passwordController.text
-                                  ? 'Password does not match'
-                                  : null,
-                          obscureText: true,
-                          decoration: inputDecoration('Confirm Password'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: submitBtn('REGISTER', loading, () {
-                          if (formKey.currentState!.validate()) {
-                            setState(() {
-                              loading = true;
-                              _registerGuide();
-                            });
-                          }
-                        }),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Already have an account?'),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pushAndRemoveUntil(
-                                  MaterialPageRoute(
-                                      builder: (context) => const GuideLogin()),
-                                  (route) => false);
-                            },
-                            child: const Text('Login'),
+                                value!.isEmpty ? 'Phone is required' : null,
+                            keyboardType: TextInputType.phone,
+                            decoration: inputDecoration('Phone'),
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: TextFormField(
+                            controller: emailController,
+                            validator: (value) =>
+                                value!.isEmpty ? 'Email is required' : null,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: inputDecoration('Email'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: TextFormField(
+                            controller: passwordController,
+                            validator: (value) =>
+                                value!.isEmpty ? 'Password is required' : null,
+                            obscureText: true,
+                            decoration: inputDecoration('Password'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: TextFormField(
+                            validator: (value) => value!.isEmpty
+                                ? 'Re-enter the password'
+                                : value != passwordController.text
+                                    ? 'Password does not match'
+                                    : null,
+                            obscureText: true,
+                            decoration: inputDecoration('Confirm Password'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: submitBtn('REGISTER', loading, () {
+                            if (formKey.currentState!.validate()) {
+                              setState(() {
+                                loading = true;
+                                _registerGuide();
+                              });
+                            }
+                          }),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Already have an account?'),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const GuideLogin()),
+                                    (route) => false);
+                              },
+                              child: const Text('Login'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
